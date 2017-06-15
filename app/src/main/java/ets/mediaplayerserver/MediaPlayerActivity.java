34177093ed9edc06966fafb6a6b38de5780fd575
Gabriel Trahan <godmaster017@gmail.com>
2017-06-15 02:37:36 -0400

    new

--- app/src/main/java/ets/mediaplayerserver/MediaPlayerActivity.java
+++ app/src/main/java/ets/mediaplayerserver/MediaPlayerActivity.java
@@ -1,9 +1,15 @@
 package ets.mediaplayerserver;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.util.TypedValue;
 import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,61 +18,17 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Field;
 
 import fi.iki.elonen.NanoHTTPD;
 
 public class MediaPlayerActivity extends AppCompatActivity {
 
+    String TAG = "mps";
     MediaPlayer player;
-    Boolean shuffling = false;
-
-    public void play(View view) {
-        Button button = (Button) view;
-        if (player.isPlaying()){
-            button.setText(R.string.play);
-            player.pause();
-        }else{
-            button.setText(R.string.pause);
-            player.start();
-        }
-        Log.d("Test", String.format("Play: %b",player.isPlaying() ));
-    }
-
-    public void next(View view) {
-//        player.getCurrentPosition();
-//                player.getSelectedTrack();
-//        player.getTrackInfo();
-//        player.selectTrack(2);
-        Log.d("Test", "Next was clicked");
-    }
-
-    public void back(View view) {
-        player.seekTo(0);
-
-        Log.d("Test", "Back was clicked");
-    }
-
-    public void loop(View view) {
-        if (player.isLooping()) {
-            player.setLooping(false);
-        }
-        else {
-            player.setLooping(true);
-        }
-        Log.d("Test", String.format("Loop: %b",player.isLooping() ));
-    }
-
-    public void shuffle(View view) {
-        if (shuffling) {
-            shuffling = false;
-        }
-        else {
-            shuffling = true;
-        }
-        Log.d("Test", String.format("Shuffling: %b",shuffling ));
-    }
-
+    Playlist playlist;
+    boolean isLooping;
 
     public static String listRaw()
     {
@@ -74,29 +36,228 @@ public class MediaPlayerActivity extends AppCompatActivity {
 
         for (int i = 0; i < fields.length - 1; i++) {
             String name = fields[i].getName();
-            Log.d("Test", String.format("Shuffling: %b",name));
+            Log.d("Test", String.format("Shuffling: %b", name));
         }
         return "bob";
+    }
+
+    public String getPath(int id)
+    {
+        TypedValue value = new TypedValue();
+        getResources().getValue(id, value, true);
+        if (value == null) {
+            return "Not Found";
+        }
+        String[] path = value.string.toString().split("/");
+
+        return "/songs/" + path[path.length - 1];
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
+        playlist = new Playlist(getApplicationContext());
+        player = MediaPlayer.create(this, playlist.getCurrentSong());
 
-        player = MediaPlayer.create(this ,R.raw.shrekanthem);
+        MediaServer server = new MediaServer(5000) {
+            @Override
+            public String getMime(int id) {
+                return getMimeDetail(id);
+            }
 
-        MediaServer server = new MediaServer(8080);
+            @Override
+            public String getPlaylist() {
+                String json = "{\n" +
+                        "\"songs\": [\n";
+                for (Integer i : playlist.playlist) {
+                    json += getSongDetails(i);
+                }
+                json += "]\n" +
+                        "} \n";
+                return json;
+            }
+
+            @Override
+            public String getId(int id) {
+                return getPath(id);
+            }
+
+            @Override
+            public void playCommand(int id) {
+                playlist.setCurrentSong(id);
+                playNewSong();
+            }
+
+
+            @Override
+            public void resumeCommand() {
+                if (!player.isPlaying()) {
+                    player.start();
+                }
+                Log.d(TAG, String.format("Play: %b", player.isPlaying()));
+            }
+
+            @Override
+            public void pauseCommand() {
+                if (player.isPlaying()) {
+                    player.pause();
+                }
+                Log.d(TAG, String.format("Play: %b", player.isPlaying()));
+            }
+
+            @Override
+            public void stopCommand() {
+                if (player.isPlaying()) {
+                    player.stop();
+                }
+                Log.d(TAG, String.format("Play: %b", player.isPlaying()));
+            }
+
+            @Override
+            public int nextCommand() {
+                playNextSong();
+                Log.d("Test", "Next was clicked");
+                return playlist.getCurrentSong();
+            }
+
+            @Override
+            public int backCommand() {
+                if (player.getCurrentPosition() > 5000) {
+                    player.seekTo(0);
+                } else {
+                    playPreviousSong();
+                }
+                Log.d("Test", "Back was clicked");
+                return playlist.getCurrentSong();
+            }
+
+            @Override
+            public boolean repeatCommand() {
+                player.setLooping(!isLooping);
+                isLooping = !isLooping;
+                Log.d("Test", String.format("Loop: %b", player.isLooping()));
+                return player.isLooping();
+            }
+
+            @Override
+            public boolean shuffleCommand() {
+                boolean isShuffled = playlist.toggleShuffle();
+                Log.d("Test", String.format("Shuffling: %b", playlist.isShuffled));
+                return isShuffled;
+            }
+
+            @Override
+            public void changeVolume(float value) {
+
+                player.setVolume(value,value);
+            }
+
+            @Override
+            public InputStream getSong(String path) {
+
+                int songId = getIDFromPath(path);
+                //return "id:" + songId;
+                return getResources().openRawResource(songId);
+            }
+        };
         try {
             server.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        TextView tx = (TextView ) findViewById(R.id.IP);
+        TextView tx = (TextView) findViewById(R.id.IP);
         tx.setText(server.toString());
 
     }
 
+    public int getIDFromPath(String path)
+    {
+        String[] songName = path.split("\\.");
+
+        Context context = getApplicationContext();
+        return context.getResources().getIdentifier(songName[0], "raw", context.getPackageName());
+    }
+
+    public void playNextSong() {
+        if (!isLooping)
+        {
+        player.reset();
+        player = MediaPlayer.create(this, playlist.getNextSong());
+        player.start();
+
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                playNextSong();
+            }
+        });
+        }else
+        {
+            playNewSong();
+        }
+    }
+
+    public void playNewSong() {
+
+            player.reset();
+            player = MediaPlayer.create(this, playlist.getCurrentSong());
+            player.start();
+
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    playNextSong();
+                }
+            });
+
+
+    }
+
+    public void playPreviousSong() {
+        player.reset();
+        player = MediaPlayer.create(this, playlist.getPreviousSong());
+        player.start();
+
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                playNextSong();
+            }
+        });
+    }
+
+    private String getSongDetails(int id) {
+
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+
+        Uri myUri = Uri.parse("android.resource://" + getPackageName() + "/" + id);
+        retriever.setDataSource(this, myUri);
+
+        String songName = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        String artistName = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUMARTIST);
+        String albumName = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+        String length = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        return "{\n" +
+                "\t\t\"ID\": \"" + id + "\",\n" +
+                "\t\t\"title\": \"" + songName + "\",\n" +
+                "\t\t\"length\": \"" + length + "\",\n" +
+                "\t\t\"artist\": \"" + artistName + "\",\n" +
+                "\t\t\"album\": \"" + albumName + "\",\n" +
+                "\t\t\"image\": \"null\"\n" +
+                "\t\t\"path\":\"" + getPath(id) + "\"\n" +
+                "\t}";
+    }
+
+    private String getMimeDetail(int id) {
+
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+
+        Uri myUri = Uri.parse("android.resource://" + getPackageName() + "/" + id);
+        retriever.setDataSource(this, myUri);
+
+        return retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_MIMETYPE);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -126,8 +287,7 @@ public class MediaPlayerActivity extends AppCompatActivity {
     }
 
 
-    public class MediaServer extends NanoHTTPD
-    {
+    public abstract class MediaServer extends NanoHTTPD {
 
         public MediaServer(int port) {
             super(port);
@@ -140,58 +300,127 @@ public class MediaPlayerActivity extends AppCompatActivity {
 
         public String parseURI(String uri)
         {
-            return "string";
+            String[] params = uri.split("/");
+            Log.d(TAG, "serve() returned: " + params.length);
+            if (params.length > 1) {
+                return "/" + params[1];
+            }
+            return "/null";
         }
 
         public String getValue(String uri)
         {
-            return "0";
+            Log.d(TAG, "value: " + uri.split("/").length);
+            String[] params = uri.split("/");
+            for (String s : params
+                    ) {
+                Log.d(TAG, s);
+            }
+            Log.d(TAG, "length:" + params[2]);
+            if (params.length > 2) {
+                return params[2];
+            }
+            return "-1";
         }
+
 
         @Override
         public Response serve(IHTTPSession session) {
-            if (session.getMethod() == Method.GET)
-            {
+            if (session.getMethod() == Method.GET) {
                 String uri = session.getUri();
+                Log.d(TAG, "uri: " + uri);
                 String command = parseURI(uri);
+                Log.d(TAG, "serve() returned: " + command);
+                if (command.equals("/playlist")) {
+                    String playlist = getPlaylist();
+                    return new Response(Response.Status.OK, MIME_PLAINTEXT, playlist);
+                } else if (command.equals("/id")) {
+                    int value = Integer.valueOf(getValue(uri));
+                    if (value != -1) {
+                        String song = getId(value);
+                        if (song.equals("NotFound")) {
+                            return new Response(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "song not found");
+                        }
+                        return new Response(Response.Status.OK, MIME_PLAINTEXT, song);
+                    }
+                    return new Response(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "id invalid");
 
-                if(command.equals("playlist"))
-                {
-
-                }else if(command.equals("id"))
-                {
-                    String value = getValue(uri);
-
-                }else if(command.equals("play"))
-                {
-
-                }else if(command.equals("pause"))
-                {
-
-                }else if(command.equals("stop"))
-                {
-
-                }else if(command.equals("next"))
-                {
-
-                }else if(command.equals("back"))
-                {
-
-                }else if(command.equals("repeat"))
-                {
-
-                }else if(command.equals("shuffle"))
-                {
-
-                }else if(command.equals("volume"))
-                {
-                    String value = getValue(uri);
+                }else if (command.equals("/play")) {
+                    int value = Integer.valueOf(getValue(uri));
+                    if (value != -1) {
+                        String song = getId(value);
+                        playCommand(value);
+                        if (song.equals("NotFound")) {
+                            return new Response(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "song not found");
+                        }
+                        return new Response(Response.Status.OK, MIME_PLAINTEXT, song);
+                    }
+                    return new Response(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "id invalid");
                 }
+                else if (command.equals("/resume")) {
+                    resumeCommand();
+                    return new Response(Response.Status.OK, MIME_PLAINTEXT, "" + playlist.getCurrentSong());
+                } else if (command.equals("/pause")) {
+                    pauseCommand();
+                    return new Response(Response.Status.OK, MIME_PLAINTEXT, "" + playlist.getCurrentSong());
+                } else if (command.equals("/stop")) {
+                    stopCommand();
+                    return new Response(Response.Status.OK, MIME_PLAINTEXT, "" + playlist.getCurrentSong());
+                } else if (command.equals("/next")) {
+                    nextCommand();
+                    return new Response(Response.Status.OK, MIME_PLAINTEXT, "" + playlist.getCurrentSong());
+                } else if (command.equals("/back")) {
+                    backCommand();
+                    return new Response(Response.Status.OK, MIME_PLAINTEXT, "" + playlist.getCurrentSong());
+                } else if (command.equals("/repeat")) {
+                    repeatCommand();
+                    return new Response(Response.Status.OK, MIME_PLAINTEXT, "Repeat:" + isLooping);
+                } else if (command.equals("/shuffle")) {
+                    boolean shuffle = shuffleCommand();
+                    return new Response(Response.Status.OK, MIME_PLAINTEXT, "Shuffle:" + shuffle);
+                } else if (command.equals("/volume")) {
+                    float value = Float.valueOf(getValue(uri));
+                    changeVolume(value);
+                    return new Response(Response.Status.OK, MIME_PLAINTEXT, "" + value);
+                } else if (command.equals("/songs")) {
+                    String value = getValue(uri);
 
+                    InputStream data = getSong(value);
+
+                    return new Response(Response.Status.OK, getMime(getIDFromPath(value)), data);
+                }/*
 
                 return new Response("<http><head><title>404</title></head><body><h1>Error 404</h1></body></http>");
+            */
             }
             return super.serve(session);
+
         }
+
+        public abstract String getMime(int id);
+        public abstract String getPlaylist();
+
+        public abstract String getId(int id);
+
+        public abstract void playCommand(int id);
+
+        public abstract void resumeCommand();
+        public abstract void pauseCommand();
+
+        public abstract void stopCommand();
+
+        public abstract int nextCommand();
+
+        public abstract int backCommand();
+
+        public abstract boolean repeatCommand();
+
+        public abstract boolean shuffleCommand();
+
+        public abstract void changeVolume(float value);
+
+        public abstract InputStream getSong(String path);
+
+
     }
 }
---
+++ app/src/main/java/ets/mediaplayerserver/Playlist.java
@@ -0,0 +1,106 @@
+package ets.mediaplayerserver;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.media.MediaMetadataRetriever;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Created by Pierre-Luc on 2017-06-08.
+ */
+
+public class Playlist {
+
+    public ArrayList<Integer> playlist;
+    private int songPlaying = 0;
+    public Boolean isShuffled = false;
+    private Context context;
+
+    public Playlist(Context applicationContext) {
+        context = applicationContext;
+        setPlaylist();
+    }
+
+    public void setPlaylist() {
+        ArrayList<Integer> musicList = new ArrayList<>();
+        Field[] fields = R.raw.class.getFields();
+
+        for (int i = 0; i < fields.length; i++) {
+            String songName = fields[i].getName();
+
+            Log.d("TestSong", i + " " + songName + " type:" + fields[i].getGenericType());
+            if (!songName.equals("$change") && !songName.equals("serialVersionUID")) {
+                int songId = context.getResources().getIdentifier(songName, "raw", context.getPackageName());
+                musicList.add(songId);
+            }
+        }
+
+        //SI CA CRASH, REMPLACE LA BOUCLE FOR PAR LE VIEUX CODE CI-DESSOUS
+//        musicList.add(R.raw.shrekanthem);
+//        musicList.add(R.raw.rockabye);
+//        musicList.add(R.raw.shapeofyou);
+
+        playlist = musicList;
+    }
+
+    public void setRandomPlaylist() {
+        Collections.shuffle(playlist);
+    }
+
+    public int getCurrentSong() {
+        return playlist.get(songPlaying);
+    }
+
+    public void setCurrentSong(int id)
+    {
+        songPlaying = playlist.indexOf(id);
+    }
+
+    public int getNextSong() {
+        if (songPlaying >= playlist.size() - 1) {
+            songPlaying = 0;
+        }
+        else {
+            songPlaying++;
+        }
+        return playlist.get(songPlaying);
+    }
+
+    public int getPreviousSong() {
+        if (songPlaying == 0) {
+            songPlaying = playlist.size() - 1;
+        }
+        else {
+            songPlaying--;
+        }
+        return playlist.get(songPlaying);
+    }
+
+    public boolean toggleShuffle() {
+        if (isShuffled) {
+            isShuffled = false;
+            setPlaylist();
+        }
+        else {
+            isShuffled = true;
+            setRandomPlaylist();
+
+        }
+        return isShuffled;
+    }
+
+    public void setIsShuffled(Boolean shuffled) {
+        isShuffled = shuffled;
+    }
+
+    public int size() {
+        return playlist.size();
+    }
+
+
+}
